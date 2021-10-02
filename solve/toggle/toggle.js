545fe2737181c767f27fb33d1ac4d40cfd0cd63a
Demi <=>
2021-10-02 10:08:02 +0100

    my solution to the toggle task

--- solve/toggle/toggle.js
+++ solve/toggle/toggle.js
@@ -3,6 +3,21 @@ export const ToggleModule = () => {
 
     //Write solution to the Toggle task inside this ToggleModule function.
     console.log('checking toggle...');
-    
+
+    let butn = document.querySelector("#switch");
+    let indicator = document.querySelector("#indicator");
+
+    //a check to see if the switch is on or off and display appropriately
+    let on_Check = () => {
+        for(let i=0; i<indicator.classList.length; i++){
+            if(indicator.classList[i] === "on"){indicator.innerText = "ON"}
+            else{indicator.innerText = "OFF"};
+        };
+    };
+
+    butn.addEventListener("click", () => {
+        indicator.classList.toggle("on");
+        on_Check();
+    });
 }
 
