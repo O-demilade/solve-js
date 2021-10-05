2e3879619e1fe2797d66c3f75230041a452269b6
Demi <=>
2021-10-05 21:19:20 +0100

    added my solution to the filter task

--- solve/filter/filter.js
+++ solve/filter/filter.js
@@ -2,5 +2,31 @@
 export const FilterModule = () => {
 
     //Write solution to the Filter task inside this FilterModule function.
+    
+    //link all control buttons
+    let allOnBtn = document.querySelector("#allColr");
+    let onlyRedBtn = document.querySelector("#onlyRed");
+    let onlyYlwBtn = document.querySelector("#onlyYlw");
+    let onlyGrnBtn = document.querySelector("#onlyGrn");
 
+    allOnBtn.addEventListener("click", ()=>{
+        document.querySelectorAll(".colrDiv").forEach(element=>element.style.display="flex")
+    });
+
+    onlyRedBtn.addEventListener("click", ()=>{
+        document.querySelectorAll(".colrDiv").forEach(element=>element.style.display="none"), 
+        document.querySelectorAll(".red").forEach(element=>element.style.display="flex")
+    });
+
+    onlyYlwBtn.addEventListener("click", ()=>{
+        document.querySelectorAll(".colrDiv").forEach(element=>element.style.display="none"), 
+        document.querySelectorAll(".yellow").forEach(element=>element.style.display="flex")
+    });
+
+    onlyGrnBtn.addEventListener("click", ()=>{
+        document.querySelectorAll(".colrDiv").forEach(element=>element.style.display="none"), 
+        document.querySelectorAll(".green").forEach(element=>element.style.display="flex")
+    });
+
+    [].forEach
 }
